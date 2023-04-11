41ecd494c32e7505855aa1f622ad896723717190
SergeyKhokhryakov <hso@list.ru>
2023-04-11 21:01:26 +0300

    Added EventFiringDecorator, Listener, Screenshot

--- java-example/src/test/java/ru/stqa/training/selenium/exercise14/WindowTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/exercise14/WindowTest.java
@@ -2,9 +2,7 @@ package ru.stqa.training.selenium.exercise14;
 
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.PageLoadStrategy;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -13,11 +11,15 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.Browser;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.safari.SafariOptions;
+import org.openqa.selenium.support.events.*;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
 import java.time.Duration;
-
-import static org.assertj.core.util.IterableUtil.toArray;
 
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -30,24 +32,54 @@ public class WindowTest {
   //@EnabledOnOs(OS.MAC)
   private int quantity;
 
+  public static class MyListener implements WebDriverListener {
+
+    @Override
+    public void beforeFindElement(WebDriver driver, By locator) {
+      System.out.println(locator);
+    }
+
+    @Override
+    public void afterFindElement(WebDriver driver, By locator, WebElement result) {
+      System.out.println(locator +  " found");
+    }
+    @Override
+    public void onError(Object target, Method method, Object[] args, InvocationTargetException e) {
+      System.out.println(e);
+      File tempFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+      File screen = new File("screen-" + System.currentTimeMillis() + ".png");
+      try {
+        Files.copy(tempFile.toPath(), screen.toPath());
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+      System.out.println(screen);
+    }
+
+  }
+
   @BeforeAll
   static public void start() {
     browser = Browser.CHROME;
     if (browser.equals(Browser.FIREFOX)) {
       FirefoxOptions options = new FirefoxOptions();
       options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-      driver = new FirefoxDriver(options);
-      driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+      driver = new EventFiringDecorator(new MyListener()).decorate(new FirefoxDriver(options));
+      //driver = new FirefoxDriver(options);
+      //driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
     } else if (browser.equals(Browser.CHROME)) {
       //Для версии Chrome (chromedriver) 111.0.5563.64 (Официальная сборка), (x86_64)
       ChromeOptions options = new ChromeOptions();
       options.addArguments("--remote-allow-origins=*");
-      driver = new ChromeDriver(options);
+      driver = new EventFiringDecorator(new MyListener()).decorate(new ChromeDriver(options));
+      //driver = new ChromeDriver(options);
     } else if (browser.equals(Browser.EDGE)) {
-      driver = new EdgeDriver();
+      driver = new EventFiringDecorator(new MyListener()).decorate(new EdgeDriver());
+      //driver = new EdgeDriver();
     } else if (browser.equals(Browser.SAFARI)) {
       SafariOptions options = new SafariOptions();
-      driver = new SafariDriver(options);
+      driver = new EventFiringDecorator(new MyListener()).decorate(new SafariDriver(options));
+      //driver = new SafariDriver(options);
     }
     driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
     wait = new WebDriverWait(driver, Duration.ofSeconds(10));
