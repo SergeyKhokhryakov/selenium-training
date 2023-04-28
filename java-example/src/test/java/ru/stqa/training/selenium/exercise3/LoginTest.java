7447a84e160ce696b9a98f461ddac9f79378ccfc
SergeyKhokhryakov <hso@list.ru>
2023-04-28 20:36:27 +0300

    Refactoring LeftPanelTest, LoginTest

--- java-example/src/test/java/ru/stqa/training/selenium/exercise3/LoginTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/exercise3/LoginTest.java
@@ -1,65 +1,16 @@
 package ru.stqa.training.selenium.exercise3;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.PageLoadStrategy;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.Browser;
-import org.openqa.selenium.safari.SafariDriver;
-import org.openqa.selenium.safari.SafariOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import ru.stqa.training.selenium.TestBase;
 
-import java.time.Duration;
+public class LoginTest extends TestBase {
 
-public class LoginTest {
-  public static WebDriver driver;
-  //public static SafariDriver driver;
-  public static WebDriverWait wait;
-  public static Browser browser;
-  //@EnabledOnOs(OS.MAC)
-  @BeforeAll
-  static public void start() {
-    browser = Browser.SAFARI;
-    if (browser.equals(Browser.FIREFOX)){
-      FirefoxOptions options = new FirefoxOptions();
-      options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-      driver = new FirefoxDriver(options);
-      driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-    } else if (browser.equals(Browser.CHROME)){
-      //Для версии Chrome (chromedriver) 111.0.5563.64 (Официальная сборка), (x86_64)
-      ChromeOptions options = new ChromeOptions();
-      options.addArguments("--remote-allow-origins=*");
-      driver = new ChromeDriver(options);
-    } else if (browser.equals(Browser.EDGE)){
-      driver = new EdgeDriver();
-    } else if (browser.equals(Browser.SAFARI)){
-      SafariOptions options = new SafariOptions();
-      driver = new SafariDriver(options);
-    }
-    if (browser != Browser.FIREFOX) {
-      wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-    }
-  }
   @Test
   public void loginTest() {
     driver.get("http://litecart.stqa.ru");
-    driver.findElement(By.cssSelector("input[name='email']")).sendKeys("litecarts@gmail.com");
-    driver.findElement(By.cssSelector("input[name='password']")).sendKeys("1234567890");
-    driver.findElement(By.cssSelector("button[name='login']")).click();
-  }
-
-  @AfterAll
-  static public void stop() {
-    driver.quit();
-    driver = null;
+    loginUser("litecarts@gmail.com", "1234567890");
+    s.assertThat(driver.findElement(By.cssSelector("div [class='notice success']")).getText()).contains("logged in");
   }
 
 }
--- java-example/src/test/java/ru/stqa/training/selenium/exercise8/LeftPanelTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/exercise8/LeftPanelTest.java
@@ -14,50 +14,17 @@ import org.openqa.selenium.remote.Browser;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.safari.SafariOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import ru.stqa.training.selenium.TestBase;
 
 import java.time.Duration;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class LeftPanelTest {
-  public static WebDriver driver;
-  //public static SafariDriver driver;
-  public static WebDriverWait wait;
-  public static Browser browser;
-  private SoftAssertions s = new SoftAssertions();
-  //@EnabledOnOs(OS.MAC)
-  @BeforeAll
-  static public void start() {
-    browser = Browser.CHROME;
-    if (browser.equals(Browser.FIREFOX)){
-      FirefoxOptions options = new FirefoxOptions();
-      options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-      driver = new FirefoxDriver(options);
-      driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-    } else if (browser.equals(Browser.CHROME)){
-      //Для версии Chrome (chromedriver) 111.0.5563.64 (Официальная сборка), (x86_64)
-      ChromeOptions options = new ChromeOptions();
-      options.addArguments("--remote-allow-origins=*");
-      driver = new ChromeDriver(options);
-    } else if (browser.equals(Browser.EDGE)){
-      driver = new EdgeDriver();
-    } else if (browser.equals(Browser.SAFARI)){
-      SafariOptions options = new SafariOptions();
-      driver = new SafariDriver(options);
-    }
-    if (browser != Browser.FIREFOX) {
-      wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-    }
-  }
-  @BeforeEach
-  public void idle(){
+public class LeftPanelTest extends TestBase {
 
-  }
   @Test
   @Order(1)
   public void loginTest() {
     driver.get("http://litecart.stqa.ru");
-    driver.findElement(By.cssSelector("input[name='email']")).sendKeys("litecarts@gmail.com");
-    driver.findElement(By.cssSelector("input[name='password']")).sendKeys("1234567890");
-    driver.findElement(By.cssSelector("button[name='login']")).click();
+    loginUser("litecarts@gmail.com", "1234567890");
     s.assertThat(driver.findElement(By.cssSelector("div [class='notice success']")).getText()).contains("logged in");
   }
 
@@ -109,18 +76,8 @@ public class LeftPanelTest {
   @Test
   @Order(6)
   public void logoutTest() {
-    driver.findElement(By.cssSelector(".content .list-vertical a[href*='logout']")).click();
+    logout();
     s.assertThat(driver.findElement(By.cssSelector("#notices i")).getText()).isSubstringOf("logged out");
-  }
-  @AfterEach
-  public void home(){
-    s.assertAll();
-    driver.findElement(By.cssSelector("#site-menu .general-0 a")).click();
-  }
-  @AfterAll
-  static public void stop() {
-    driver.quit();
-    driver = null;
   }
 
 }
