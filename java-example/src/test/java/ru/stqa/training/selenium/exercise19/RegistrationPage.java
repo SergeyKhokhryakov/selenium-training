c04f38567ab7ee0c21f13b39e69850d2b3e63894
SergeyKhokhryakov <hso@list.ru>
2023-05-03 20:20:32 +0300

    'Css' locator changed to 'name' locator

--- java-example/src/test/java/ru/stqa/training/selenium/exercise19/RegistrationPage.java
+++ java-example/src/test/java/ru/stqa/training/selenium/exercise19/RegistrationPage.java
@@ -4,6 +4,8 @@ import com.mifmif.common.regex.Generex;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
 public class RegistrationPage extends Page {
@@ -14,32 +16,34 @@ public class RegistrationPage extends Page {
     driver.get("http://litecart.stqa.ru");
     driver.findElement(By.cssSelector(".content a[href*='create_account']")).click();
   }
+
   public WebElement firstnameInput(){
-    return driver.findElement(By.cssSelector("input[name='firstname']"));
+    return driver.findElement(By.name("firstname"));
   }
+
   public WebElement lastnameInput(){
-    return driver.findElement(By.cssSelector("input[name='lastname']"));
+    return driver.findElement(By.name("lastname"));
   }
   public WebElement address1Input(){
-    return driver.findElement(By.cssSelector("input[name='address1']"));
+    return driver.findElement(By.name("address1"));
   }
   public WebElement cityInput(){
-    return driver.findElement(By.cssSelector("input[name='city']"));
+    return driver.findElement(By.name("city"));
   }
   public WebElement emailInput (){
-    return driver.findElement(By.cssSelector("input[name='email']"));
+    return driver.findElement(By.name("email"));
   }
   public WebElement phoneInput(){
-    return driver.findElement(By.cssSelector("input[name='phone']"));
+    return driver.findElement(By.name("phone"));
   }
   public WebElement passwordInput(){
-    return driver.findElement(By.cssSelector("input[name='password']"));
+    return driver.findElement(By.name("password"));
   }
   public WebElement confirmedPasswordInput(){
-    return driver.findElement(By.cssSelector("input[name='confirmed_password']"));
+    return driver.findElement(By.name("confirmed_password"));
   }
   public WebElement createAccountButton (){
-    return driver.findElement(By.cssSelector("button[name='create_account']"));
+    return driver.findElement(By.name("create_account"));
   }
   public WebElement logoutLink (){
     return  driver.findElement(By.cssSelector(".content .list-vertical a[href*='logout']"));
@@ -61,19 +65,19 @@ public class RegistrationPage extends Page {
     // генерация строки postcode на основе регулярного выражения, соответствующего стране country
     // если для страны не определен шаблон regexp, то значением является аргумент (customer.getPostcode()) теста
     String postCode;
-    String pattern = driver.findElement(By.cssSelector("input[name='postcode']")).getAttribute("pattern");
+    String pattern = driver.findElement(By.name("postcode")).getAttribute("pattern");
     if (pattern.length() == 0){
       postCode = postcode;
     } else {
       Generex generex = new Generex(pattern);
       postCode = generex.random();
     }
-    driver.findElement(By.cssSelector("input[name='postcode']")).sendKeys(postCode);
+    driver.findElement(By.name("postcode")).sendKeys(postCode);
   }
 
   public void inputPhone (String phone){
-    String code = driver.findElement(By.cssSelector("input[name='phone']")).getAttribute("placeholder");
-    driver.findElement(By.cssSelector("input[name='phone']")).sendKeys(code + phone);
+    String code = driver.findElement(By.name("phone")).getAttribute("placeholder");
+    driver.findElement(By.name("phone")).sendKeys(code + phone);
   }
 
 }
